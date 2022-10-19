982d99e679ae13e37157d6a507f5f129c0d45a08
Martin Nabhan <7613182+martinnabhan@users.noreply.github.com>
2022-10-19 19:45:29 +0900

    Initialise repository

---
+++ index.ts
@@ -0,0 +1,1 @@
+export * from '@apollo-server-integration-next/lib/startServerAndCreateNextHandler';
---
+++ lib/startServerAndCreateNextHandler.ts
@@ -0,0 +1,3 @@
+const startServerAndCreateNextHandler = () => null;
+
+export { startServerAndCreateNextHandler };
