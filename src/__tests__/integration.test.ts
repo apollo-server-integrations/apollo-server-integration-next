3a5137a7f04ae9bc0a2b868060de041d8b218872
Sergey Arakelov <4978697+arakelovd@users.noreply.github.com>
2023-11-05 23:09:10 -0500

    Support for Next 14 (#160)
    Co-authored-by: Martin Nabhan <7613182+martinnabhan@users.noreply.github.com>
    Co-authored-by: Trevor Scheer <trevor.scheer@gmail.com>

--- src/__tests__/integration.test.ts
+++ src/__tests__/integration.test.ts
@@ -7,20 +7,37 @@ import {
 import { createServer } from 'http';
 import { AddressInfo } from 'net';
 
-const { apiResolver } =
-  process.env.NEXT_VERSION === '12'
-    ? require('next12/dist/server/api-utils/node')
-    : require('next13/dist/server/api-utils/node');
+async function getApiResolver() {
+  let apiResolver;
+
+  switch (process.env.NEXT_VERSION) {
+    case '12':
+      ({ apiResolver } = await import('next12/dist/server/api-utils/node'));
+      break;
+    case '13':
+      ({ apiResolver } = await import('next13/dist/server/api-utils/node/api-resolver'));
+      break;
+    case '14':
+      ({ apiResolver } = await import('next14/dist/server/api-utils/node/api-resolver'));
+      break;
+    default:
+      throw new Error(
+        'Next.js version not specified. Ensure the Next.js version is specified via the NEXT_VERSION environment variable.',
+      );
+  }
+
+  return apiResolver;
+}
 
 describe('nextHandler', () => {
   defineIntegrationTestSuite(
     async (serverOptions: ApolloServerOptions<BaseContext>, testOptions?: CreateServerForIntegrationTestsOptions) => {
       const server = new ApolloServer(serverOptions);
       const handler = startServerAndCreateNextHandler(server, testOptions);
+      const apiResolver = await getApiResolver();
+      type ApiContext = Omit<Parameters<typeof apiResolver>[4], 'revalidate'>;
 
-      const httpServer = createServer((req, res) =>
-        apiResolver(req, res, '', handler, {} as Parameters<typeof apiResolver>[4], false),
-      );
+      const httpServer = createServer((req, res) => apiResolver(req, res, '', handler, {} as ApiContext, false));
 
       await new Promise<void>(resolve => {
         httpServer.listen({ port: 0 }, resolve);
